d8af0b41f58287b3d01be0d7a8bdaf828b2192fe
Haicang <aseaboat@outlook.com>
2018-05-25 23:15:16 +0800

    haven't debug gaussian mixture

--- 3-gaussian-mixture/gm.py
+++ 3-gaussian-mixture/gm.py
@@ -1,7 +1,10 @@
 
 
 import numpy as np
+from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
+
+import copy
 
 
 def generate_data():
@@ -33,12 +36,12 @@ def distortion(X, label, mu):
     """
     To compute \sum ||x_i - mu_{c^i}||^2 in k-means
     """
-    sum = 0
+    S = 0
     m = X.shape[0]
     for i in range(m):
         d = X[i, :] - mu[label[i], :]
-        sum += np.inner(d, d)
-    return sum
+        S += np.inner(d, d)
+    return S
 
 
 def tag(X, mu):
@@ -59,19 +62,7 @@ def tag(X, mu):
                 dist = tmp
                 c[i] = j
 
-    return np.int32(c)
-
-
-def update_mu(X, c, mu):
-    """
-    To update mu in K-means
-    """
-    k = mu.shape[1]
-    for j in range(k):
-        indices = (c == j)
-        count = np.sum(indices)
-        mu[j, :] = np.sum(X[indices, :], axis=0) / count
-    return mu
+    return c.astype(np.int32)
 
 
 class KMeans():
@@ -84,6 +75,18 @@ class KMeans():
         self.mu = None
 
     def fit(self, X):
+        
+        def update_mu(X, c, mu):
+            """
+            To update mu in K-means
+            """
+            k = mu.shape[1]
+            for j in range(k):
+                indices = (c == j)
+                count = np.sum(indices)
+                mu[j, :] = np.sum(X[indices, :], axis=0) / count
+            return mu
+
         # assert X.shape
         k = self.n
         size = X.shape[0]
@@ -112,8 +115,107 @@ class KMeans():
         return tag(X, self.mu)
 
 
+def Estep(X, phi, mu, sigma):
+    m = X.shape[0]
+    k = mu.shape[0]
+    P = np.zeros((m, k))
+
+    for j in range(k):
+        m_norm = multivariate_normal(mean=mu[j], cov=sigma[j])
+        P[:, j] = m_norm.pdf(X) * phi[j]
+    W = P / np.sum(P, axis=1).reshape(m, 1)
+
+    return W
+
+
+def Mstep(X, W, phi, mu, sigma):
+    # update phi
+    m = W.shape[0]
+    phi = np.sum(W, axis=0) / m
+
+    # update mu
+    (m, k) = W.shape
+    for j in range(k):
+        mu[j] = np.sum(W[:, j] * X, axis=0) / np.sum(W[:, j], axis=0)
+
+    # update sigma
+    for j in range(k):
+        for i in range(m):
+            d = X[i] - mu[j]
+            sigma[j] += W[i, j] * np.outer(d, d)
+        sigma[j] /= np.sum(W[:, j])
+
+    return phi, mu, sigma
+
+
 class GaussianMixture():
-    pass
+    
+    def __init__(self, n_cluster, max_iter=100, tol=0.0001):
+        self.n = n_cluster
+        self.epochs = max_iter
+        self.tol = tol
+
+        # params
+        self.phi = None
+        self.mu = None
+        self.sigma = None
+
+    def fit(self, X):
+        
+        # These functions are replaced by `Estep` and `Mstep`
+        def update_W():
+            pass
+
+        def update_phi(W):
+            m = W.shape[0]
+            return np.sum(W, axis=0) / m
+
+        def update_mu(W, X, mu):
+            (m, k) = W.shape
+            for j in range(k):
+                mu[j] = np.sum(W[:, j] * X, axis=0) / np.sum(W[:, j], axis=0)
+            return mu
+        
+        def update_sigma(W, X, mu, sigma):
+            for j in range(k):
+                for i in range(m):
+                    d = X[i] - mu[j]
+                    sigma[j] += W[i, j] * np.outer(d, d)
+                sigma[j] /= np.sum(W[:, j])
+            return sigma
+
+        m = X.shape[0]
+        n = X.shape[1]
+        k = self.n
+        
+        # Init params with k-means
+        kmeans = KMeans(k)
+        kmeans.fit(X)
+        mu = copy.deepcopy(kmeans.mu)
+
+        W = np.zeros((m, k))
+        c = kmeans.predict(X)
+        for i in range(m):
+            W[i, c[i]] = 1
+
+        phi = update_phi(W)
+
+        sigma = np.zeros((k, n, n))
+        sigma = update_sigma(W, X, mu, sigma)
+
+        # Use EM alg to iterate
+        for e in range(self.epochs):
+            W = Estep(X, phi, mu, sigma)
+            phi, mu, sigma = Mstep(X, W, phi, mu, sigma)
+
+        # Save params to class
+        self.phi = phi
+        self.mu = mu
+        self.sigma = sigma
+
+    def predict(self, X):
+        W = Estep(X, self.phi, self.mu, self.sigma)
+        return np.argmax(W, axis=1)
 
 
 if __name__ == '__main__':
