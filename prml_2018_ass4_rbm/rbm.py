7b351b282a480bbac8392b525f42e312478288dd
Haicang <aseaboat@outlook.com>
2018-06-12 22:40:05 +0800

    demo rbm

--- prml_2018_ass4_rbm/rbm.py
+++ prml_2018_ass4_rbm/rbm.py
@@ -2,6 +2,15 @@
 # encoding: utf-8
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def sample_sigmoid(prob):
+    return np.random.binomial(1, prob)
 
 
 class RBM:
@@ -9,21 +18,70 @@ class RBM:
 
     def __init__(self, n_hidden=2, n_observe=784):
         """Initialize model."""
+        self.nh = n_hidden
+        self.nv = n_observe
+        self.W = np.zeros((n_observe, n_hidden))
+        self.a = np.zeros((n_observe, 1))
+        self.b = np.zeros((n_hidden, 1))
 
-        # 请补全此处代码
-        pass
-
-    def train(self, data):
+    def train(self, data, T=10, learning_rate=0.01):
         """Train model using data."""
+        # Reshape the training data
+        data = np.reshape(data, (-1, self.nv, 1))
 
-        # 请补全此处代码
-        pass
+        # Number of training e.g.
+        N = data.shape[0]
+        # learning_rate
+        alpha = learning_rate
 
-    def sample(self):
+        # Variables of the model
+        W, a, b = self.W, self.a, self.b
+        # The hidden variables(vec)
+        h = np.zeros((self.nh, 1))
+        v = np.zeros((self.nv, 1))
+        hp = np.zeros((self.nh, 1))  # h_prime
+        vp = np.zeros((self.nv, 1))  # v_prime
+
+        for t in range(T):
+            for n in range(N):
+                assert v.shape == data[n].shape
+                v = data[n]
+
+                p_hv = sigmoid(np.dot(W.T, v) + b)
+                h = sample_sigmoid(p_hv)
+
+                pos_grad = np.dot(v, h.T)
+
+                p_vh = sigmoid(np.dot(W, h) + a)
+                vp = sample_sigmoid(p_vh)
+                
+                p_hv = sigmoid(np.dot(W.T, vp) + b)
+                hp = sample_sigmoid(p_hv)
+
+                neg_grad = np.dot(vp, hp.T)
+
+                # Update params: W, a, b
+                W += alpha * (pos_grad - neg_grad)
+                a += alpha * (v - vp)
+                b += alpha * (h - hp)
+
+        self.W, self.a, self.b = W, a, b
+
+    def sample(self, T=100):
         """Sample from trained model."""
+        W, a, b = self.W, self.a, self.b
 
-        # 请补全此处代码
-        pass
+        v = np.random.randn(self.nv, 1)
+        p_hv = sigmoid(np.dot(W.T, v) + b)
+        h = sample_sigmoid(p_hv)
+
+        for t in range(T):
+            p_vh = sigmoid(np.dot(W, h) + a)
+            v = sample_sigmoid(p_vh)
+            p_hv = sigmoid(np.dot(W.T, v) + b)
+            h = sample_sigmoid(p_hv)
+
+        return v
 
 
 # train restricted boltzmann machine using mnist dataset
@@ -34,11 +92,11 @@ if __name__ == '__main__':
     img_size = n_rows * n_cols
     print(mnist.shape)
 
-    # construct rbm model
-    rbm = RBM(2, img_size)
+    # # construct rbm model
+    # rbm = RBM(10, img_size)
 
-    # train rbm model using mnist
-    rbm.train(mnist)
+    # # train rbm model using mnist
+    # rbm.train(mnist)
 
-    # sample from rbm model
-    s = rbm.sample()
+    # # sample from rbm model
+    # s = rbm.sample()
